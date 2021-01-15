1b053001fdce7256afa6014401c3bcfe6dd4baec
Wiseman Fernandes <wizyman@gmail.com>
2021-01-15 12:34:29 +0530

    Testimonials section css hotfix

--- showcase-wp/template-parts/blocks/testimonial/testimonial.php
+++ showcase-wp/template-parts/blocks/testimonial/testimonial.php
@@ -12,34 +12,42 @@
 <?php 
 $testimonials_count = get_fields()['testimonials'];
  ?>
-
-<?php if (have_rows('testimonials')): ?>    
-<div class="testimonials">
-    <div id="testimonials_carousel" class="carousel slide col-sm-4" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <?php foreach ($testimonials_count as $key => $value) : ?>
-                <li data-target="#testimonials_carousel" data-slide-to="<?php echo $key; ?>" <?php echo $key == 0 ? "class='active'"  : ""; ?>></li>
-            <?php endforeach; ?>
-        </ol>
-        <div class="carousel-inner">
-            <?php while (have_rows('testimonials')): the_row(); ?>
-                <div class="testimonial carousel-item <?php echo get_row_index() == 1 ? 'active' : ''; ?>">
-                    <div class="d-block w-100">
-                        <div class="image">
-                            <img src="<?php echo get_the_post_thumbnail_url(get_sub_field('testimonial')->ID); ?>">
-                        </div>
-                        <div class="quote">
-                            <?php echo get_sub_field('testimonial')->post_content; ?>
-                        </div>
-                        <div class="name">
-                            <?php echo get_sub_field('testimonial')->post_title; ?>
-                        </div>
+<section class="container-fluid py-5">
+    <div class="container newjoinees text-center">
+        <h4>Our Newest Members Have This To Say</h4>
+        <h1>Members Speak</h1>
+        <div id="testimonials_carousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <?php foreach ($testimonials_count as $key => $value) : ?>
+                    <li data-target="#testimonials_carousel" data-slide-to="<?php echo $key; ?>" <?php echo $key == 0 ? "class='active'"  : ""; ?>></li>
+                <?php endforeach; ?>
+            </ol>
+            <?php if (have_rows('testimonials')): ?>    
+                <div class="row">
+                    <div class="carousel-inner">
+                        <?php while (have_rows('testimonials')): the_row(); ?>
+                            <div class="testimonial carousel-item <?php echo get_row_index() == 1 ? 'active' : ''; ?>">
+                                <div class="col-sm-12 newjoineescard shadow-lg">
+                                    <div class="row">
+                                        <div class="col-3 imgdiv">
+                                            <img src="<?php echo get_the_post_thumbnail_url(get_sub_field('testimonial')->ID); ?>" />
+                                        </div>
+                                        <div class="col-9 text-center p-3 pt-5">
+                                            <div class="secondaryCircle">
+                                                <i class="fa fa-quote-left" aria-hidden="true"></i>
+                                            </div>
+                                            <p><?php echo get_sub_field('testimonial')->post_content; ?></p>
+                                            <p class="text-secondary"><?php echo get_sub_field('testimonial')->post_title; ?></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endwhile; ?>
                     </div>
-                </div>  
-            <?php endwhile; ?>
+                </div>
+            <?php else: ?>
+                <h2>Add testimonials here</h2>
+            <?php endif;?>
         </div>
     </div>
-</div> 
-<?php else: ?>
-    <h2>Add testimonials here</h2>
-<?php endif;?>
+</section>
