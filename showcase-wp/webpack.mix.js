baf4129814643f1b018fcbbdf15052fc271ea76a
Wiseman Fernandes <wizyman@gmail.com>
2021-01-20 15:56:53 +0530

    css finally fixed

--- showcase-wp/webpack.mix.js
+++ showcase-wp/webpack.mix.js
@@ -3,7 +3,7 @@ require('laravel-mix-purgecss');
 
 mix.sass('./sass/main.scss', './dist/main.css').options({
         processCssUrls: false,
-}).sass('./bootstrap/scss/bootstrap.scss', './dist/bootstrap.css').js('./bootstrap/js/index.js', './dist/bootstrap-scripts.js').purgeCss({
+}).js('./bootstrap/js/index.js', './dist/bootstrap-scripts.js').purgeCss({
 	content:["./*.php", "./template-parts/*.php", "./template-parts/blocks/*/*.php", "./inc/*.php"],
 	safelist: require("purgecss-with-wordpress").safelist
 });
