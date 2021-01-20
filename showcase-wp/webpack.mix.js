a9d40ab89d8c1b0623d7eab766a21fdce2d4256a
Wiseman Fernandes <wizyman@gmail.com>
2021-01-20 13:26:26 +0530

    Fix gitignore for dist folder

--- showcase-wp/webpack.mix.js
+++ showcase-wp/webpack.mix.js
@@ -4,10 +4,11 @@ require('laravel-mix-purgecss');
 mix.sass('./sass/main.scss', './dist/main.css').options({
         processCssUrls: false,
 }).sass('./bootstrap/scss/bootstrap.scss', './dist/bootstrap.css').js('./bootstrap/js/index.js', './dist/bootstrap-scripts.js').purgeCss({
-	content:["./*.php", "./template-parts/*.php"],
+	content:["./*.php", "./template-parts/*.php", "./template-parts/blocks/*/*.php", "./inc/*.php"],
 	safelist: require("purgecss-with-wordpress").safelist
 });
 
 mix.autoload({ 
   'jquery': ['$', 'window.jQuery', 'jQuery'],
 });
+
