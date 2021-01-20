bf0adf7e80f2cfc8d7635b3efe61bc4609478b4b
Wiseman Fernandes <wizyman@gmail.com>
2021-01-20 17:09:28 +0530

    css fixes

--- showcase-wp/webpack.mix.js
+++ showcase-wp/webpack.mix.js
@@ -1,8 +1,9 @@
 let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 
+
 mix.sass('./sass/main.scss', './dist/main.css').options({
-        processCssUrls: false,
+    processCssUrls: false
 }).js('./bootstrap/js/index.js', './dist/bootstrap-scripts.js').purgeCss({
 	content:["./*.php", "./template-parts/*.php", "./template-parts/blocks/*/*.php", "./inc/*.php"],
 	safelist: require("purgecss-with-wordpress").safelist
