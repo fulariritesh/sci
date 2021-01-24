1cadddf2c97ccf15f4ff7aa01b25b5f5892664c3
siddharth-rsspl <Siddharth@remotesoftwaresolutions.com>
2021-01-24 16:53:15 +0530

    fixed verify email template

--- showcase-wp/ultimate-member/email/checkmail_email.php
+++ showcase-wp/ultimate-member/email/checkmail_email.php
@@ -1,15 +1,17 @@
 <div style="max-width: 560px; padding: 20px; background: #ffffff; border-radius: 5px; margin: 40px auto; font-family: Open Sans,Helvetica,Arial; font-size: 15px; color: #666;">
 <div style="color: #444444; font-weight: normal;">
-<div style="text-align: center; font-weight: 600; font-size: 26px; padding: 10px 0; border-bottom: solid 3px #eeeeee;">{site_name}</div>
-<div style="clear: both;"> </div>
+<div style="font-weight: 600; font-size: 26px; padding: 10px 0px; border-bottom: 3px solid #eeeeee; text-align: center;">{site_name}</div>
+<div>
+<p style="padding-left: 40px;">Hey, there!</p>
+<p style="padding-left: 40px;">You are one step away from joining Showcase India’s global community of talent. We are so excited to have you get started on your showcase. Please click on the button below to verify your email address so we can keep your account safe. </p>
+</div>
 </div>
 <div style="padding: 0 30px 30px 30px; border-bottom: 3px solid #eeeeee;">
-<div style="padding: 30px 0; font-size: 24px; text-align: center; line-height: 40px;">Thank you for signing up!<span style="display: block;">Please click the following link to activate your account.</span></div>
-<div style="padding: 10px 0 50px 0; text-align: center;"><a style="background: #555555; color: #fff; padding: 12px 30px; text-decoration: none; border-radius: 3px; letter-spacing: 0.3px;" href="{account_activation_link}">Activate your Account</a></div>
-<div style="padding: 15px; background: #eee; border-radius: 3px; text-align: center;">Need help? <a style="color: #3ba1da; text-decoration: none;" href="mailto:{admin_email}">contact us</a> today.</div>
+<div style="padding: 10px 0 50px 0; text-align: center;"><a style="background: #555555; color: #fff; padding: 12px 30px; text-decoration: none; border-radius: 3px; letter-spacing: 0.3px;" href="{account_activation_link}" target="_blank" rel="noopener">Verify My Account</a></div>
+<div>If you’re having trouble, please reach out to us at support@showcaseindia.com.            </div>
 </div>
 <div style="color: #999; padding: 20px 30px;">
-<div>Thank you!</div>
-<div>The <a style="color: #3ba1da; text-decoration: none;" href="{site_url}">{site_name}</a> Team</div>
+<div>Thank you,</div>
+<div>Team <a style="color: #3ba1da; text-decoration: none;" href="{site_url}">{site_name}</a></div>
 </div>
 </div>
