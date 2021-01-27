173e5e22921a11ef81416965d2ad20a2b378dbd3
Ritesh <ritesh@remotesoftwaresolutions.com>
2021-01-27 17:08:31 +0530

    filters added for category

---
+++ showcase-wp/inc/discover-talent-rest-server.php
@@ -0,0 +1,265 @@
+<?php
+ 
+class Discover_Talent_Rest_Server extends WP_REST_Controller {
+ 
+  var $my_namespace = 'sci/v';
+  var $my_version   = '1';
+ 
+  public function register_routes() {
+    $namespace = $this->my_namespace . $this->my_version;
+    $base      = 'talent-data';
+    register_rest_route( $namespace, '/' . $base, array(
+      array(
+          'methods'         => WP_REST_Server::READABLE,
+          'callback'        => array( $this, 'get_talent_data' )
+        )
+    ));
+
+    $base      = 'sub-categories-data';
+    register_rest_route( $namespace, '/' . $base, array(
+      array(
+          'methods'         => WP_REST_Server::READABLE,
+          'callback'        => array( $this, 'get_sub_categories_data' )
+        )
+    ));
+  }
+ 
+  public function hook_rest_server(){
+    add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+  }
+
+ 
+  public function get_talent_data( WP_REST_Request $request ){
+    $pageNumber = $request->get_param( 'pageNumber' );
+    $pageSize = $request->get_param( 'pageSize' );
+
+    $limit = (int)$pageSize;
+    $offset = ($pageNumber -1) * $pageSize; 
+    
+    $args = array(
+        'role'          => 'subscriber',
+        'number'        => $limit,
+        'meta_key'      => 'profession',
+        'meta_value'    => array(''),
+        'meta_compare'  => 'NOT IN',
+        'orderby'       => 'user_registered',
+        'order'         => 'DESC',
+        'offset'        => $offset,
+        'fields'        => array( 'ID','display_name' )
+    );
+    $userList = get_users($args);
+ 
+    if( empty( $userList ) ){
+    return null;
+    }
+ 
+
+    foreach($userList as $user){
+        $user->href = "/wordpress/profile/" . $user->ID;
+        $user->headshot = get_field('sci_user_headshot', 'user_' . $user->ID);
+        $user->location = get_field('sci_user_location', 'user_' . $user->ID);
+
+        $termIds = get_user_meta($user->ID, 'profession',true);
+
+        $userProfessions = get_terms( array(
+            'include'       => $termIds,
+            'hide_empty'    => false,
+            'parent'        => 0,
+            'fields'        => 'ids'
+        ) );
+
+        $user->professions = array();
+        foreach($userProfessions as $profession){
+            array_push($user->professions, get_term_meta( $profession, 'category_name_singular', true ));
+        }
+        
+    }
+    
+    $data = new stdClass();
+    $data->userList = array($userList);
+
+    if($pageNumber == 1){
+        $args = array(
+            'role'          => 'subscriber',
+            'meta_key'      => 'profession',
+            'meta_value'    => array(''),
+            'meta_compare'  => 'NOT IN',
+            'orderby'       => 'user_registered',
+            'order'         => 'DESC',
+            'fields'        => array( 'ID')
+        );
+
+        $data->totalCount = count(get_users($args));
+
+        $categoriesFound = get_terms( array(
+            'taxonomy'      => 'jobs',
+            'hide_empty'    => false,
+            'parent'        => 0,
+            'fields'        => 'id=>name'
+        ) );
+
+        $categories = array();
+        foreach($categoriesFound as $key => $value){
+            $category = new stdClass();
+            $category->id = $key;
+            $category->name = $value;
+            $category->image = get_field('sci_category_image', 'jobs_' . $key);
+
+            $args = array(
+              'role'          => 'subscriber',
+              'orderby'       => 'user_registered',
+              'order'         => 'DESC',
+              'fields'        => array( 'ID','display_name' ),
+              'meta_query'     => [
+                'relation' => 'AND',
+                [
+                    'key'     => 'profession',
+                    'value'   => array(''),
+                    'compare' => 'NOT IN',
+                ],
+                [
+                    'key'     => 'profession',
+                    'value'   => sprintf(':"%s";', $key),
+                    'compare' => 'LIKE',
+                ],
+            ],
+          );
+          $category->count = count(get_users($args));
+
+          array_push($categories, $category);
+        }
+
+        $data->categories = $categories;
+    }
+
+    return $data;
+  }
+
+  public function get_sub_categories_data( WP_REST_Request $request ){
+    $pageNumber = $request->get_param( 'pageNumber' );
+    $pageSize = $request->get_param( 'pageSize' );
+    $categoryId = $request->get_param( 'category' );
+    $categoryIds = explode(",", $request->get_param( 'category' ));  
+
+    $limit = (int)$pageSize;
+    $offset = ($pageNumber -1) * $pageSize; 
+    
+
+
+    // $meta_query = array( 'relation' => 'AND' );
+
+    // foreach ( $categoryIds as $key => $value ) {
+    //     $meta_query[] = array( 'key' => $key, 'value' => $value );
+    // }
+
+    // return array(
+    //     'post_type'  => array( 'post', 'custom' ),
+    //     'meta_query' => $meta_query,
+    // );
+
+    $args = array(
+        'role'          => 'subscriber',
+        'number'        => $limit,
+        'orderby'       => 'user_registered',
+        'order'         => 'DESC',
+        'offset'        => $offset,
+        'fields'        => array( 'ID','display_name' ),
+        'meta_query'     => [
+          'relation' => 'AND',
+          [
+              'key'     => 'profession',
+              'value'   => array(''),
+              'compare' => 'NOT IN',
+          ],
+          [
+              'key'     => 'profession',
+              'value'   =>  sprintf(':"%s";', $categoryId),
+              'compare' => 'LIKE',
+          ],
+      ],
+    );
+    $userList = get_users($args);
+ 
+    if( empty( $userList ) ){
+      return null;
+    }
+ 
+
+    foreach($userList as $user){
+
+        $termIds = get_user_meta($user->ID, 'profession',true);
+       
+        $userProfessions = get_terms( array(
+            'include'       => $termIds,
+            'hide_empty'    => false,
+            'parent'        => 0,
+            'fields'        => 'ids'
+        ) );
+
+        $user->professions = array();
+        foreach($userProfessions as $profession){
+            array_push($user->professions, get_term_meta( $profession, 'category_name_singular', true ));
+        }
+
+        $user->href = "/wordpress/profile/" . $user->ID;
+        $user->headshot = get_field('sci_user_headshot', 'user_' . $user->ID);
+        $user->location = get_field('sci_user_location', 'user_' . $user->ID);    
+        $user->ids = $categoryIds;    
+    }
+    
+    $data = new stdClass();
+    $data->userList = array($userList);
+
+    if($pageNumber == 1){
+        $totalUsersArgs = array(
+            'role'          => 'subscriber',
+            'orderby'       => 'user_registered',
+            'order'         => 'DESC',
+            'fields'        => array( 'ID','display_name' ),
+            'meta_query'     => [
+              'relation' => 'AND',
+              [
+                  'key'     => 'profession',
+                  'value'   => array(''),
+                  'compare' => 'NOT IN',
+              ],
+              [
+                  'key'     => 'profession',
+                  'value'   => sprintf(':"%s";', $categoryId),
+                  'compare' => 'LIKE',
+              ],
+          ],
+        );
+
+        $totalUsers = get_users($totalUsersArgs);
+        
+        $data->totalCount = count($totalUsers);
+
+        $categoriesFound = get_terms( array(
+            'taxonomy'      => 'jobs',
+            'hide_empty'    => false,
+            'parent'        => $categoryId,
+            'fields'        => 'id=>name'
+        ) );
+
+        $subcategories = array();
+        foreach($categoriesFound as $key => $value){
+            $category = new stdClass();
+            $category->id = $key;
+            $category->name = $value;
+            $category->image = get_field('sci_category_image', 'jobs_' . $key);
+            array_push($subcategories, $category);
+        }
+
+        $data->subcategories = $subcategories;
+    }
+
+    return $data;
+  }
+ 
+}
+ 
+$discover_talent_rest_server = new Discover_Talent_Rest_Server();
+$discover_talent_rest_server->hook_rest_server();
+
+?>
---
+++ showcase-wp/templates/template-discover-talent.php
@@ -0,0 +1,116 @@
+<?php
+/*
+Template Name: Discover Talent
+*/
+?>
+
+ <?php get_header(); ?>
+ <div class="bodyBG">
+
+
+ <section class="container-fluid findtalent-topbar">
+    <div class="container px-0 ">
+      <div class="row">
+        <div class="col-6 col-sm-8 pt-3">
+          <!-- Nav tabs -->
+          <ul class="nav findtalent-tabs">
+            <li class="nav-item">
+              <a class="nav-link select" data-toggle="tab" href="">Find Talent</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-6 col-sm-4 pt-3 scibreadcrumb">
+          <ul class="nav float-right">
+            <li class="nav-item">
+              <a class="nav-link select" data-toggle="tab" href="#category-search" id="AllCategories">All Categories</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="tab-content">
+        <div class="tab-pane container active category-search p-3" id="category-search">
+          <div class="FT-allcategories" id="FT-allcategories">
+          <h5 class="text-uppercase">All Categories</h5>
+          <div class="row m-0 category-blocks">
+            
+          </div>
+          </div>
+          <div class="FT-subcategories" id="FT-subcategories">
+            <h5 class="text-uppercase">Actor</h5>
+            <div class="row m-0 sub-category-blocks">
+           
+            </div>
+            </div>
+        </div>
+
+        <div class="searchFilters mt-2">
+          <div class="p-3 d-block d-md-none">
+            <span class="d-block d-md-none float-left">Advance Filter</span>
+            <button class="btn btn-primary btn-sm d-block d-md-none float-right" type="button" data-toggle="collapse"
+              data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+              <i class="fas fa-filter"></i>
+            </button>
+            <div class="clear"></div>
+          </div>
+
+          <div class="searchparam shadow-sm collapse dont-collapse-sm " id="collapseExample">
+            <div class=" row well px-3">
+              <!-- Collapse Panel -->
+              <div class="col-12 col-sm-6 col-md-3 py-3">
+                <select placeholder="Select Location" class="form-control">
+                  <option>Delhi</option>
+                </select>
+              </div>
+              <div class="col-12 col-sm-6 col-md-2 py-3"> <select placeholder="Select Location" class="form-control">
+                  <option>Delhi</option>
+                </select></div>
+              <div class="col-12 col-sm-6 col-md-2 py-3">
+                <select placeholder="Select Location" class="form-control">
+                  <option>Delhi</option>
+                </select>
+              </div>
+              <div class="col-12 col-sm-6 col-md-2 py-3">
+                <select placeholder="Select Location" class="form-control">
+                  <option>Delhi</option>
+                </select>
+              </div>
+              <div class="col-12 col-sm-6 col-md-2 py-3">
+                <input type="text" class="form-control" placeholder="Search...">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="searchResults my-3">
+
+    <div class="container  ">
+      <div class="row noResults  p-0 shadow-sm ">
+        <div class="col-6 col-sm-6 text-right pt-3"><img src="http://localhost:8080/wordpress/wp-content/uploads/2021/01/noProfilesFound.jpg" alt="No profile found" class="img-fluid" /></div>
+        <div class="col-6 col-sm-6 p-3">
+          <h4>No Profiles found from Pondicherry</h4>
+          <h6>Explore other locations nearby for your requirements</h6>
+          <h6 class="pt-1 pt-sm-2">If you want to showcase your talent from this location</h6>
+          <button class="btn btn-sm btn-primary">Join Now</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="container resultlisting my-4">
+      <div class="row">
+      <div class="col-6 px-md-0">Showcasing India’s Talent Nationwide</div>
+        <div class="col-6 px-md-0 text-right"><span class="totalCount">0</span> Results</div>
+      </div>
+      <div class="row py-3 displayList">
+        
+                
+      </div>
+      <div class="loadMore text-center py-3"><button class="btn btn-md btn-full btn-primary px-5">Load More</button></div>
+    </div>
+  </section>
+</div>
+    
+<?php get_footer(); ?>
