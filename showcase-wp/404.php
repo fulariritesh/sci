2b8661d61327f1f246f2623e3696dad71bbc956f
Amey Volvoikar <AmeyVolvoikar@remotesoftwaresolutions.com>
2021-02-19 12:41:37 +0530

    404 page and h1 changed to h2 on homepage

--- showcase-wp/404.php
+++ showcase-wp/404.php
@@ -14,43 +14,14 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'showcase' ); ?></h1>
+				<h1 class="page-title text-center py-5 px-2"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'showcase' ); ?></h1>
 			</header><!-- .page-header -->
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'showcase' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'showcase' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$showcase_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'showcase' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$showcase_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+			<div class="page-content four-o-four">
+				<h1 class="text-center mb-5">404</h1>
+				<div class="text-center mb-5">
+				<a class="btn btn-outline-primary btn-lg btn-xs text-center" href="<?php echo get_page_link(get_page_by_path('homepage'))?>">Go to Homepage</a>
+				</div>
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
 
